a4955cd62487841026978e046de00dc991d1610b
TajbeerAhamed <mdtajbeerahamedrimon@gmail.com>
2022-10-26 00:30:07 +0600

    data-added-to-cart

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 const Course = ({course}) => {
-    console.log(course);
+    const {id,name} = course
     return (
         <div>
-            <h2>This is Course {course.length}</h2>
+           <div className="max-w-xs p-6 rounded-md shadow-md bg-gray-900 text-gray-50">
+	<img src="https://source.unsplash.com/random/300x300/?1" alt="" className="object-cover object-center w-full rounded-md h-72 bg-gray-500" />
+	<div className="mt-6 mb-2">
+		<span className="block text-xs font-medium tracking-widest uppercase text-violet-400">{name}</span>
+		<h2 className="text-xl font-semibold tracking-wide">Nam maximus purus</h2>
+	</div>
+	<p className="text-gray-100">Mauris et lorem at elit tristique dignissim et ullamcorper elit. In sed feugiat mi. Etiam ut lacinia dui.</p>
+</div>
         </div>
     );
 };
